80d488b8031d0c27b811dfd9c4ebd549f3dbd0c7
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2023-07-25 19:25:40 +0200

    tests: add coverage

--- unit-tests/wormhole.test.js
+++ unit-tests/wormhole.test.js
@@ -1,55 +1,75 @@
 // @ts-check
 
-import { before, describe, it } from "node:test";
+import { after, before, describe, it } from "node:test";
+import fs from "node:fs";
 import assert from "node:assert/strict";
-import { main } from "obscurity-sdk";
+import path from "node:path";
+import initVM from "obscurity-sdk";
 import { Cl } from "@stacks/transactions";
-import { mainnet_valid_guardians_set_upgrades, mainnet_valid_pfs } from "./constants.js";
+import { mainnet_valid_guardians_set_upgrades } from "./constants.js";
+
+let wormhole_core_v1_contract_name = "wormhole-core-v1";
+let wormhole_core_v1_contract_addr;
+let deployer;
+let sender;
+/** @type import("../../clarinet/components/clarinet-sdk/dist/index").ClarityVM */
+let vm;
 
 describe("Wormhole testsuite", () => {
-  const cost = 1000000n;
-  let wormhole_core_v1_contract_name = "wormhole-core-v1";
-  let wormhole_core_v1_contract_addr;
-  let deployer;
-  let sender;
-  /** @type import("obscurity-sdk").Session */
-  let session;
-
   before(async () => {
-    session = await main();
-    await session.initSession(process.cwd(), "./Clarinet.toml");
-    
-    const accounts = session.getAccounts();
+    vm = await initVM();
+    await vm.initSession(process.cwd(), "./Clarinet.toml");
+
+    const accounts = vm.getAccounts();
     deployer = accounts.get("deployer");
     wormhole_core_v1_contract_addr = `${deployer}.${wormhole_core_v1_contract_name}`;
     sender = accounts.get("wallet_1");
   });
 
-  it("ensure that guardians set can be rotated", () => {
+  after(() => {
+    const lcov = vm.terminate();
+    fs.appendFileSync(path.join(process.cwd(), "lcov.info"), lcov);
+  });
 
+  it("ensure that guardians set can be rotated", () => {
     const vaaRotation1 = Cl.bufferFromHex(mainnet_valid_guardians_set_upgrades[0].vaa);
     let publicKeysRotation1 = [];
     for (let key of mainnet_valid_guardians_set_upgrades[0].keys) {
-        publicKeysRotation1.push(Cl.bufferFromHex(key));
+      publicKeysRotation1.push(Cl.bufferFromHex(key));
     }
 
     const vaaRotation2 = Cl.bufferFromHex(mainnet_valid_guardians_set_upgrades[1].vaa);
     let publicKeysRotation2 = [];
     for (let key of mainnet_valid_guardians_set_upgrades[1].keys) {
-        publicKeysRotation2.push(Cl.bufferFromHex(key));
+      publicKeysRotation2.push(Cl.bufferFromHex(key));
     }
 
     const vaaRotation3 = Cl.bufferFromHex(mainnet_valid_guardians_set_upgrades[2].vaa);
     let publicKeysRotation3 = [];
     for (let key of mainnet_valid_guardians_set_upgrades[2].keys) {
-        publicKeysRotation3.push(Cl.bufferFromHex(key));
+      publicKeysRotation3.push(Cl.bufferFromHex(key));
     }
 
-    session.callPublicFn(wormhole_core_v1_contract_name, "update-guardians-set", [vaaRotation1, Cl.list(publicKeysRotation1)], sender);
+    vm.callPublicFn(
+      wormhole_core_v1_contract_name,
+      "update-guardians-set",
+      [vaaRotation1, Cl.list(publicKeysRotation1)],
+      sender
+    );
 
-    session.callPublicFn(wormhole_core_v1_contract_name, "update-guardians-set", [vaaRotation2, Cl.list(publicKeysRotation2)], sender);
+    vm.callPublicFn(
+      wormhole_core_v1_contract_name,
+      "update-guardians-set",
+      [vaaRotation2, Cl.list(publicKeysRotation2)],
+      sender
+    );
 
-    session.callPublicFn(wormhole_core_v1_contract_name, "update-guardians-set", [vaaRotation3, Cl.list(publicKeysRotation3)], sender);
+    vm.callPublicFn(
+      wormhole_core_v1_contract_name,
+      "update-guardians-set",
+      [vaaRotation3, Cl.list(publicKeysRotation3)],
+      sender
+    );
 
     // // assert: review returned data, contract state, and other requirements
     // assertEquals(block.receipts.length, 3);
