ed9b8ffac4d63cb0ffc5758fedf03ce44a72fd5f
Ludo Galabru <ludo@hiro.so>
2023-12-11 11:49:07 -0500

    chore: add e2e ptgm tests

--- unit-tests/pyth/ptgm.test.ts
+++ unit-tests/pyth/ptgm.test.ts
@@ -1,19 +1,80 @@
-import { Cl } from "@stacks/transactions";
+import { Cl, ClarityType } from "@stacks/transactions";
 import { beforeEach, describe, expect, it } from "vitest";
 import { wormhole } from "../wormhole/helpers";
 import { pyth } from "./helpers";
 import { hexToBytes } from "@noble/hashes/utils";
+import { ParsedTransactionResult } from "@hirosystems/clarinet-sdk";
+import { ptgmTestnetVaas } from "./fixtures";
 
 const pythOracleContractName = "pyth-oracle-v1";
 const pythStorageContractName = "pyth-store-v1";
 const pythDecoderPnauContractName = "pyth-pnau-decoder-v1";
 const pythGovernanceContractName = "pyth-governance-v1";
 const wormholeCoreContractName = "wormhole-core-v1";
+const initialFeeRecipient = "ST3CRXBDXQ2N5P7E25Q39MEX1HSMRDSEAP1JST19D";
+
+describe("pyth-governance-v1::update-fee-value mainnet VAAs", () => {
+  const accounts = simnet.getAccounts();
+  const sender = accounts.get("wallet_1")!;
+
+  let block: ParsedTransactionResult[] | undefined = undefined;
+
+  // Before starting the test suite, we have to setup the guardian set.
+  beforeEach(async () => {
+    block = wormhole.applyMainnetGuardianSetUpdates(
+      sender,
+      wormholeCoreContractName,
+    );
+  });
+
+  it("should succeed handling the 3 guardians rotations", () => {
+    expect(block!).toHaveLength(3);
+    block!.forEach((b: ParsedTransactionResult) => {
+      expect(b.result).toHaveClarityType(ClarityType.ResponseOk);
+    });
+  });
+
+  it("should succeed handling PNAU mainnet payloads", () => {
+    const pnauBytes = Cl.bufferFromHex(ptgmTestnetVaas[0]);
+
+    const wormholeContract = Cl.contractPrincipal(
+      simnet.deployer,
+      wormholeCoreContractName,
+    );
+
+    let res = simnet.callPublicFn(
+      pythGovernanceContractName,
+      "update-fee-value",
+      [pnauBytes, wormholeContract],
+      sender,
+    ).result;
+
+    expect(res).toBeOk(Cl.tuple({
+      exponent: Cl.uint(0),
+      mantissa: Cl.uint(2),
+    }));
+
+    let read = simnet.callReadOnlyFn(
+      pythGovernanceContractName,
+      `get-fee-info`,
+      [],
+      sender,
+    );
+
+    expect(Cl.ok(read.result)).toBeOk(
+      Cl.tuple({
+        address: Cl.standardPrincipal(initialFeeRecipient),
+        exponent: Cl.uint(0),
+        mantissa: Cl.uint(2),
+      }),
+    );
+  });
+});
+
 
 describe("pyth-governance-v1::update-fee-value", () => {
   const accounts = simnet.getAccounts();
   const sender = accounts.get("wallet_1")!;
-  const initialFeeRecipient = "ST3CRXBDXQ2N5P7E25Q39MEX1HSMRDSEAP1JST19D";
   const guardianSet = wormhole.generateGuardianSetKeychain(19);
   let updateFeeValue = {
     mantissa: 2n,
